94de9f2c713b3175c6ca6300ec501ecfa85b2d68
henrypp <sforce5@mail.ru>
2015-11-29 21:11:26 +0600

    1.1 released

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,8 @@
+v1.1 (29 November 2015)
++ code rewritten on c++
++ added select for downloading architecture
++ added new settings (see chrlauncher.ini)
++ more information about update
+
 v1.0 (26 November 2015)
 - first public version
---
+++ src/resource.h
@@ -0,0 +1,49 @@
+#ifndef __RESOURCE_H__
+#define __RESOURCE_H__
+
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+// Menu Id
+#define IDM_MAIN 100
+
+// Dialogs
+#define IDD_ABOUT 100
+#define IDD_MAIN 101
+
+// About Dlg
+#define IDC_LOGO 100
+#define IDC_TITLE 101
+#define IDC_COPYRIGHT 102
+#define IDC_LINKS 103
+#define IDC_INFO 104
+
+// Main Dlg
+#define IDC_PROGRESS 1001
+#define IDC_VERSION 1002
+#define IDC_REVISION 1003
+#define IDC_BUILD 1004
+#define IDC_COMMIT 1005
+#define IDC_DATE 1006
+#define IDC_STATUSBAR 1007
+
+// Main Menu
+#define IDM_EXIT 1000
+#define IDM_WEBSITE 1001
+#define IDM_ABOUT 1002
+
+// Icons
+#define IDI_MAIN 100
+
+// Strings
+#define IDS_VERSION 1000
+#define IDS_REVISION 1001
+#define IDS_BUILD 1002
+#define IDS_COMMIT 1003
+#define IDS_DATE 1004
+
+#define IDS_STATUS_DOWNLOAD 1005
+#define IDS_STATUS_INSTALL 1006
+
+#endif // __RESOURCE_H__
