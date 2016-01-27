e4bd2a0e2041cbd737d568c9cc6ce7380c5c1d8b
henrypp <sforce5@mail.ru>
2016-01-27 09:54:52 +0600

    added mailto registration to script

--- bin/SetDefaultBrowser.bat
+++ bin/SetDefaultBrowser.bat
@@ -24,21 +24,22 @@ if not exist "%CHRLAUNCHER_PATH%" (
 		reg add "HKLM\Software\Classes\chrlauncherHTML\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
 		reg add "HKLM\Software\Classes\chrlauncherHTML\shell\open\command" /v "" /t REG_SZ /d "%CHRLAUNCHER_ARGS%" /f
 
-		reg add "HKLM\Software\Classes\chrlauncherURL" /v "" /t REG_SZ /d "Chromium Document" /f
+		reg add "HKLM\Software\Classes\chrlauncherURL" /v "" /t REG_SZ /d "chrlauncher document" /f
 		reg add "HKLM\Software\Classes\chrlauncherURL" /v "URL Protocol" /t REG_SZ /d "" /f
 		reg add "HKLM\Software\Classes\chrlauncherURL\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
 		reg add "HKLM\Software\Classes\chrlauncherURL\shell\open\command" /v "" /t REG_SZ /d "%CHRLAUNCHER_ARGS%" /f
 
 		reg add "HKLM\Software\RegisteredApplications" /v "chrlauncher" /t REG_SZ /d "Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities" /f
 
+		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE" /v "" /t REG_SZ /d "chrlauncher" /f
+
 		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
 
 		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\shell\open\command" /v "" /t REG_SZ /d "\"%CHRLAUNCHER_PATH%\"" /f
 
 		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities" /v "ApplicationIcon" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities" /v "ApplicationName" /t REG_SZ /d "Chromium" /f
-
-		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities\StartMenu" /v "StartMenuInternet" /t REG_SZ /d "CHRLAUNCHER.EXE" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities" /v "ApplicationName" /t REG_SZ /d "chrlauncher" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities" /v "ApplicationDescription" /t REG_SZ /d "Portable Chromium Launcher" /f
 
 		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities\FileAssociations" /v ".htm" /t REG_SZ /d "chrlauncherHTML" /f
 		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities\FileAssociations" /v ".html" /t REG_SZ /d "chrlauncherHTML" /f	
@@ -49,6 +50,7 @@ if not exist "%CHRLAUNCHER_PATH%" (
 		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities\URLAssociations" /v "ftp" /t REG_SZ /d "chrlauncherURL" /f
 		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities\URLAssociations" /v "http" /t REG_SZ /d "chrlauncherURL" /f
 		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities\URLAssociations" /v "https" /t REG_SZ /d "chrlauncherURL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\CHRLAUNCHER.EXE\Capabilities\URLAssociations" /v "mailto" /t REG_SZ /d "chrlauncherURL" /f
 
 	)
 )
