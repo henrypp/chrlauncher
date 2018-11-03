ba7ab2f1a6c6a16a1512ed04e551d680697df0fa
henrypp <support@henrypp.org>
2018-11-03 09:25:01 +0600

    upd ids

--- src/resource.hpp
+++ src/resource.hpp
@@ -25,46 +25,52 @@
 #define IDC_STATUSBAR 108
 
 // Main Menu
-#define IDM_EXIT 109
-#define IDM_WEBSITE 110
-#define IDM_ABOUT 111
+#define IDM_RUN 109
+#define IDM_EXIT 110
+#define IDM_WEBSITE 111
+#define IDM_ABOUT 112
 
 // Tray Menu
-#define IDM_TRAY_SHOW 112
-#define IDM_TRAY_WEBSITE 113
-#define IDM_TRAY_ABOUT 114
-#define IDM_TRAY_EXIT 115
+#define IDM_TRAY_SHOW 113
+#define IDM_TRAY_RUN 114
+#define IDM_TRAY_WEBSITE 115
+#define IDM_TRAY_ABOUT 116
+#define IDM_TRAY_EXIT 117
+
+#define IDX_LANGUAGE 1000
 
 // Icons
 #define IDI_MAIN 100
 
 // Strings
 #define IDS_FILE 1
-#define IDS_EXIT 2
-#define IDS_HELP 3
-#define IDS_WEBSITE 4
-#define IDS_ABOUT 5
-#define IDS_DONATE 6
+#define IDS_RUN 2
+#define IDS_EXIT 3
+#define IDS_SETTINGS 4
+#define IDS_LANGUAGE 5
+#define IDS_HELP 6
+#define IDS_WEBSITE 7
+#define IDS_ABOUT 8
+#define IDS_DONATE 9
 
-#define IDS_TRAY_SHOW 7
+#define IDS_TRAY_SHOW 10
 
-#define IDS_BROWSER 8
-#define IDS_CURRENTVERSION 9
-#define IDS_VERSION 10
-#define IDS_DATE 11
+#define IDS_BROWSER 11
+#define IDS_CURRENTVERSION 12
+#define IDS_VERSION 13
+#define IDS_DATE 14
 
-#define IDS_ACTION_DOWNLOAD 12
-#define IDS_ACTION_INSTALL 13
+#define IDS_ACTION_DOWNLOAD 15
+#define IDS_ACTION_INSTALL 16
 
-#define IDS_STATUS_CHECK 14
-#define IDS_STATUS_DOWNLOAD 15
-#define IDS_STATUS_INSTALL 16
-#define IDS_STATUS_FOUND 17
-#define IDS_STATUS_DOWNLOADED 18
-#define IDS_STATUS_ERROR 19
+#define IDS_STATUS_CHECK 17
+#define IDS_STATUS_DOWNLOAD 18
+#define IDS_STATUS_INSTALL 19
+#define IDS_STATUS_FOUND 20
+#define IDS_STATUS_DOWNLOADED 21
 
-#define IDS_QUESTION_STOP 20
+#define IDS_QUESTION_STOP 22
 
-#define IDS_CLOSE 21
+#define IDS_CLOSE 23
 
 #endif // __RESOURCE_H__
