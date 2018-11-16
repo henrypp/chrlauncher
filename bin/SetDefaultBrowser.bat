55362bae74a96141e947006a37bd1fd48f4676e6
henrypp <support@henrypp.org>
2018-11-16 09:35:25 +0600

    upd install script

--- bin/SetDefaultBrowser.bat
+++ bin/SetDefaultBrowser.bat
@@ -1,12 +1,15 @@
 @echo off
 
+set "CHRLAUNCHER_NAME=chrlauncher"
+set "CHRLAUNCHER_NAME_SHORT=chrlauncher"
+set "CHRLAUNCHER_DESCRIPTION=Chromium portable launcher and updater"
 set "CHRLAUNCHER_PATH=%~dp0chrlauncher.exe"
 set "CHRLAUNCHER_ICON=\"%CHRLAUNCHER_PATH%\",0"
 set "CHRLAUNCHER_ARGS=\"%CHRLAUNCHER_PATH%\" \"%%1\""
 
 if not exist "%CHRLAUNCHER_PATH%" (
 
-	echo ERROR: "chrlauncher.exe" not found.
+	echo ERROR: "%CHRLAUNCHER_PATH%" not found.
 
 ) else (
 
@@ -20,53 +23,54 @@ if not exist "%CHRLAUNCHER_PATH%" (
 
 		regedit /s "%~dp0RegistryCleaner.reg"
 
-		reg add "HKLM\Software\Classes\chrlauncherHTML" /v "" /t REG_SZ /d "Chromium HTML Document" /f
-		reg add "HKLM\Software\Classes\chrlauncherHTML\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
-		reg add "HKLM\Software\Classes\chrlauncherHTML\shell\open\command" /v "" /t REG_SZ /d "%CHRLAUNCHER_ARGS%" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%HTML" /v "" /t REG_SZ /d "%CHRLAUNCHER_NAME% Document" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%HTML\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%HTML\shell\open\command" /v "" /t REG_SZ /d "%CHRLAUNCHER_ARGS%" /f
 
-		reg add "HKLM\Software\Classes\chrlauncherURL" /v "" /t REG_SZ /d "Chromium URL Protocol" /f
-		reg add "HKLM\Software\Classes\chrlauncherURL" /v "EditFlags" /t REG_DWORD /d "2" /f
-		reg add "HKLM\Software\Classes\chrlauncherURL" /v "FriendlyTypeName" /t REG_SZ /d "Chromium URL" /f
-		reg add "HKLM\Software\Classes\chrlauncherURL" /v "URL Protocol" /t REG_SZ /d "" /f
-		reg add "HKLM\Software\Classes\chrlauncherURL\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
-		reg add "HKLM\Software\Classes\chrlauncherURL\shell\open\command" /v "" /t REG_SZ /d "%CHRLAUNCHER_ARGS%" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%URL" /v "" /t REG_SZ /d "%CHRLAUNCHER_NAME% Protocol" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%URL" /v "EditFlags" /t REG_DWORD /d "2" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%URL" /v "FriendlyTypeName" /t REG_SZ /d "%CHRLAUNCHER_NAME% Protocol" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%URL" /v "URL Protocol" /t REG_SZ /d "" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%URL\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
+		reg add "HKLM\Software\Classes\%CHRLAUNCHER_NAME_SHORT%URL\shell\open\command" /v "" /t REG_SZ /d "%CHRLAUNCHER_ARGS%" /f
 
-		reg add "HKLM\Software\RegisteredApplications" /v "chrlauncher" /t REG_SZ /d "Software\Clients\StartMenuInternet\chrlauncher\Capabilities" /f
+		reg add "HKLM\Software\RegisteredApplications" /v "%CHRLAUNCHER_NAME_SHORT%" /t REG_SZ /d "Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities" /f
 
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher" /v "" /t REG_SZ /d "chrlauncher" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%" /v "" /t REG_SZ /d "%CHRLAUNCHER_NAME%" /f
 
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\DefaultIcon" /v "" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
 
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\shell\open\command" /v "" /t REG_SZ /d "\"%CHRLAUNCHER_PATH%\"" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\shell\open\command" /v "" /t REG_SZ /d "\"%CHRLAUNCHER_PATH%\"" /f
 
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\InstallInfo" /v "IconsVisible" /t REG_DWORD /d "1" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\InstallInfo" /v "IconsVisible" /t REG_DWORD /d "1" /f
 
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities" /v "ApplicationIcon" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities" /v "ApplicationName" /t REG_SZ /d "chrlauncher" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities" /v "ApplicationDescription" /t REG_SZ /d "Chromium portable launcher and updater" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities" /v "ApplicationIcon" /t REG_SZ /d "%CHRLAUNCHER_ICON%" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities" /v "ApplicationName" /t REG_SZ /d "%CHRLAUNCHER_NAME%" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities" /v "ApplicationDescription" /t REG_SZ /d "%CHRLAUNCHER_DESCRIPTION%" /f
 
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\FileAssociations" /v ".htm" /t REG_SZ /d "chrlauncherHTML" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\FileAssociations" /v ".html" /t REG_SZ /d "chrlauncherHTML" /f	
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\FileAssociations" /v ".shtml" /t REG_SZ /d "chrlauncherHTML" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\FileAssociations" /v ".xht" /t REG_SZ /d "chrlauncherHTML" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\FileAssociations" /v ".xhtml" /t REG_SZ /d "chrlauncherHTML" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\FileAssociations" /v ".htm" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%HTML" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\FileAssociations" /v ".html" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%HTML" /f	
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\FileAssociations" /v ".shtml" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%HTML" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\FileAssociations" /v ".xht" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%HTML" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\FileAssociations" /v ".xhtml" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%HTML" /f
 
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\StartMenu" /v "StartMenuInternet" /t REG_SZ /d "chrlauncher" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\StartMenu" /v "StartMenuInternet" /t REG_SZ /d "%CHRLAUNCHER_NAME%" /f
 
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "ftp" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "http" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "https" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "mailto" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "webcal" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "urn" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "tel" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "smsto" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "sms" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "nntp" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "news" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "mms" /t REG_SZ /d "chrlauncherURL" /f
-		reg add "HKLM\Software\Clients\StartMenuInternet\chrlauncher\Capabilities\URLAssociations" /v "irc" /t REG_SZ /d "chrlauncherURL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "ftp" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "http" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "https" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "mailto" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "webcal" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "urn" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "tel" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "smsto" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "sms" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "nntp" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "news" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "mms" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
+		reg add "HKLM\Software\Clients\StartMenuInternet\%CHRLAUNCHER_NAME_SHORT%\Capabilities\URLAssociations" /v "irc" /t REG_SZ /d "%CHRLAUNCHER_NAME_SHORT%URL" /f
 
+		%windir%\system32\control.exe /name Microsoft.DefaultPrograms /page pageDefaultProgram\pageAdvancedSettings?pszAppName=%CHRLAUNCHER_NAME_SHORT%
 	)
 )
 
