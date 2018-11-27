78c4a3f4113ed5750a6618d3fe5952450370b5c8
henrypp <support@henrypp.org>
2018-11-27 16:32:32 +0600

    upd version string

--- src/app.hpp
+++ src/app.hpp
@@ -6,8 +6,8 @@
 
 #define APP_NAME L"chrlauncher"
 #define APP_NAME_SHORT L"chrlauncher"
-#define APP_VERSION L"2.5.2"
-#define APP_VERSION_RES 2,5,2,0
+#define APP_VERSION L"2.5.3"
+#define APP_VERSION_RES 2,5,3,0
 #define APP_COPYRIGHT L"(c) 2015-2018 " _APP_AUTHOR L". All Rights Reversed."
 
 #endif // __APP_H__
