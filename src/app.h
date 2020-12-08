a57d4734b5731fd2811384e1a4f796de657fd468
henrypp <support@henrypp.org>
2020-12-09 01:23:02 +0600

    upd app version information

---
+++ src/app.h
@@ -0,0 +1,10 @@
+// chrlauncher
+// Copyright (c) 2015-2021 Henry++
+
+#pragma once
+
+#define APP_NAME L"chrlauncher"
+#define APP_NAME_SHORT L"chrlauncher"
+#define APP_VERSION L"2.6"
+#define APP_VERSION_RES 2,6,0,0
+#define APP_COPYRIGHT L"(c) 2015-2021 " APP_AUTHOR L". All Rights Reversed."
