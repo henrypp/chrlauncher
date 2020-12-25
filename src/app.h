8202d6d55eb4417cd51583801a89b8da85eb560c
henrypp <support@henrypp.org>
2020-12-26 02:14:30 +0600

    upd project sdk

--- src/app.h
+++ src/app.h
@@ -5,6 +5,7 @@
 
 #define APP_NAME L"chrlauncher"
 #define APP_NAME_SHORT L"chrlauncher"
+#define APP_AUTHOR L"Henry++"
 #define APP_VERSION L"2.6"
 #define APP_VERSION_RES 2,6,0,0
 #define APP_COPYRIGHT L"(c) 2015-2021 " APP_AUTHOR L". All Rights Reversed."
