1c9c91638d0ba8bfbbe8e4c50de07cf9b557d889
henrypp <support@henrypp.org>
2021-01-08 17:32:11 +0600

    upd resource builder script

--- build_resources.bat
+++ build_resources.bat
@@ -1,7 +1,7 @@
 @echo off
 
-php "..\builder\make_resource.php" ".\src\resource.hpp"
-php "..\builder\make_locale.php" "chrlauncher" "chrlauncher" ".\bin\i18n" ".\src\resource.hpp" ".\src\resource.rc" ".\bin\chrlauncher.lng"
+php "..\builder\make_resource.php" ".\src\resource.h"
+php "..\builder\make_locale.php" "chrlauncher" "chrlauncher" ".\bin\i18n" ".\src\resource.h" ".\src\resource.rc" ".\bin\chrlauncher.lng"
 copy /y ".\bin\chrlauncher.lng" ".\bin\32\chrlauncher.lng"
 copy /y ".\bin\chrlauncher.lng" ".\bin\64\chrlauncher.lng"
 
