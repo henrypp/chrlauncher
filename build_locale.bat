eca50589d1c3c9539e7ad6f9d6117b6c84f604c3
henrypp <support@henrypp.org>
2021-04-08 18:14:12 +0600

    upd build script

--- build_locale.bat
+++ build_locale.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd ..\builder
-call build chrlauncher 2.5.7
+call build_locale chrlauncher
 
 pause
--- build_resources.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-php "..\builder\make_resource.php" ".\src\resource.h"
-php "..\builder\make_locale.php" "chrlauncher" "chrlauncher" ".\bin\i18n" ".\src\resource.h" ".\src\resource.rc" ".\bin\chrlauncher.lng"
-copy /y ".\bin\chrlauncher.lng" ".\bin\32\chrlauncher.lng"
-copy /y ".\bin\chrlauncher.lng" ".\bin\64\chrlauncher.lng"
-
-pause
