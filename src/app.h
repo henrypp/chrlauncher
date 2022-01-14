d6c9e4236181b6d79303c1a60781b204d535219e
henrypp <support@henrypp.org>
2022-01-14 22:47:40 +0600

    ++year

--- src/app.h
+++ src/app.h
@@ -1,5 +1,5 @@
 // chrlauncher
-// Copyright (c) 2015-2021 Henry++
+// Copyright (c) 2015-2022 Henry++
 
 #pragma once
 
@@ -8,4 +8,4 @@
 #define APP_AUTHOR L"Henry++"
 #define APP_VERSION L"2.6"
 #define APP_VERSION_RES 2,6,0,0
-#define APP_COPYRIGHT L"(c) 2015-2021 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2015-2022 " APP_AUTHOR L". All Rights Reversed."
