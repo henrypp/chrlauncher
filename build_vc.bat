77837c58d361b7ec19794145117d5489425fece0
henrypp <support@henrypp.org>
2023-10-22 13:40:23 +0600

    upd build vc

--- build_vc.bat
+++ build_vc.bat
@@ -18,13 +18,10 @@ goto end
 
 :start
 
-msbuild simplewall.sln -property:Configuration=Release -property:Platform=x86 -verbosity:normal
+msbuild chrlauncher.sln -property:Configuration=Release -property:Platform=x86 -verbosity:normal
 if %ERRORLEVEL% neq 0 goto end
 
-msbuild simplewall.sln -property:Configuration=Release -property:Platform=x64 -verbosity:normal
-if %ERRORLEVEL% neq 0 goto end
-
-msbuild simplewall.sln -property:Configuration=Release -property:Platform=ARM64 -verbosity:normal
+msbuild chrlauncher.sln -property:Configuration=Release -property:Platform=x64 -verbosity:normal
 if %ERRORLEVEL% neq 0 goto end
 
 :end
