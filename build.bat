7296fa15ef2aca354fe713c164fe171e69b4fcfe
localhaos <li.anonimes@gmail.com>
2026-07-06 00:46:23 +0000

    force update

---
+++ build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd ..\builder
+call build chrlauncher 2.7
+
+pause
---
+++ src/app.h
@@ -0,0 +1,11 @@
+// chrlauncher
+// Copyright (c) 2015-2026 Henry++
+
+#pragma once
+
+#define APP_NAME L"chrlauncher"
+#define APP_NAME_SHORT L"chrlauncher"
+#define APP_AUTHOR L"Henry++"
+#define APP_VERSION L"2.7"
+#define APP_VERSION_RES 2,7,0,0
+#define APP_COPYRIGHT L"(c) 2015-2026 " APP_AUTHOR L". All Rights Reversed."
